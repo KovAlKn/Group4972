b1ad780a72abd21338e45f13caad7d8b7adfaa33
Aleksei Kovalev <akovalevgm@mail.ru>
2023-09-04 22:47:39 +0400

    solution from sem1 task 0,1,3,5

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача №0
+//Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+
+Console.WriteLine ("Введите число: ");
+//Считываем данные с консоли 
+string? inputNum = Console.ReadLine();//??"0";
+//Проверяем, чтобы данные были не пустыми
+if(inputNum!=null)
+{
+    // Парсим введеное число
+    // int num = int.Parse(inputNum);
+    // Находим квадрат числа
+    // int res = num * num;
+    // Ввыводим данные в консоль
+    // Console.WriteLine(res);
+}
+
+Console.WriteLine("Квадрат числа: "+Math.Pow(int.Parse(inputNum),2));
---
+++ Sem1Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача №1
+// Напишите программу, которая на вход принимает два числа и проверяет,
+//  является ли первое число квадратом второго.
+
+Console.WriteLine("Напишите первое число");
+int num1 = int.Parse(Console.ReadLine()??"0");
+
+Console.WriteLine("Напишите второе число");
+int num2 = int.Parse(Console.ReadLine()??"0");
+
+// Console.WriteLine(num1==Math.Pow(num2,2)?"да":"нет"); - не работает, разобрать почему
