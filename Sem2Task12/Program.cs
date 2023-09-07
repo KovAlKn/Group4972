5d40f01aa1c678fa2c23df428d131f1d7316942f
Aleksei Kovalev <akovalevgm@mail.ru>
2023-09-07 22:33:17 +0400

    added task9, task11, task12

---
+++ Sem2Task12/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача №12
+// Напишите программу, которая будет принимать на вход два числа и выводить,
+// является ли второе число кратным первому. Если второе число некратно первому,
+//  то программа выводит остаток от деления.
+
+
+Console.WriteLine("Введите первое число ");
+int firstNum = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Введите второе число ");
+int secondNum = int.Parse(Console.ReadLine()??"0");
+if (secondNum%firstNum ==0)
+{
+    Console.WriteLine("Второе число кратно первому");
+}
+else
+{
+    Console.WriteLine("Второе не кратно первому. Остаток от оделения "+secondNum%firstNum);
+}
