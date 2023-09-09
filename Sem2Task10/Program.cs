b90147542d84808f3c954a806e09fb86edd7f17e
Aleksei Kovalev <akovalevgm@mail.ru>
2023-09-09 20:16:51 +0400

    added task 10 and task13

---
+++ Sem2Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿// №10 Напишите программу, которая принимает на вход трёхзначное число
+//  и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число");
+string line = Console.ReadLine()??"0";
+if (line.Length == 3)
+{
+    char[] digits = line.ToCharArray();
+    Console.WriteLine(digits[1]);
+}
+else
+{
+    Console.WriteLine("Введено не трехзначное число");
+}
---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,24 @@
+﻿// №13 Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число");
+double num = double.Parse(Console.ReadLine()??"0");
+if (num>9999999999)
+{
+    Console.WriteLine("число содержит больше 10 цифр");
+}
+else if (num >=100)
+{
+    int step = 1;
+    
+    while (num / Math.Pow(10,step) > 9)
+    {
+            step++;
+    }
+    double thirdNum = (num - (num % Math.Pow(10, step-2))) / Math.Pow(10, step-2) % 10;
+    Console.Write("Третья цифра: ");
+    Console.Write(thirdNum);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
