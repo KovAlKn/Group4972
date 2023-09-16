8a22e1cae578b09f3bb0416a542ea454436ab607
Aleksei Kovalev <akovalevgm@mail.ru>
2023-09-16 18:34:28 +0400

    added tasks 25 and 27

---
+++ Sem4Task25/Program.cs
@@ -0,0 +1,97 @@
+﻿// №25 Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// Вводим данные
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+// Печатаем результат
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+// Метод если выбрана операция:+,-, *,^. Деление вынесено отдельно.
+long Calculator(int firstNum, int secondNum, string mathOperation)
+{
+    long result = 0;
+    if(mathOperation == "+")
+    {
+        result = mySum(firstNum, secondNum);
+    }
+
+    else if (mathOperation == "-")
+    {
+    result = myMinus(firstNum, secondNum); 
+    }
+    else if(mathOperation == "*")
+    {
+        result = myMultiplication(firstNum, secondNum);
+    }
+    else if(mathOperation == "^")
+    {
+    result = MyPow(firstNum, secondNum);
+    }
+    return result;
+}
+
+long MyPow(int numA, int numB)
+{
+    long res = 1;
+    while(numB > 0)
+    {
+        res = res * numA;
+        numB = numB - 1;
+    }
+    return res;
+}
+
+int mySum(int numA, int numB)
+{
+    int res = numA + numB;
+    return res;
+}
+
+int myMinus(int numA, int numB)
+{
+    int res = numA-numB;
+    return res;
+}
+
+long myMultiplication(int numA, int numB)
+{
+   long res = numA * numB;
+    return res;
+}
+float myDivision(float numA, float numB)
+{
+    float res = numA / numB;
+    return res;
+}
+
+// Получение первого числа
+int firstNum =ReadData("Введите число первое число ");
+
+// Выбор математической операции
+Console.WriteLine("Выберете операцию: +|-|*|/|^|");
+string mathOperation = Console.ReadLine()??"0";
+
+// получение второго числа
+int secondNum =ReadData("Введите второе число");
+
+// Вычисление
+if (mathOperation== "/")
+{
+    float result = myDivision(Convert.ToSingle(firstNum),Convert.ToSingle(secondNum));
+    PrintResult(firstNum + mathOperation + secondNum +" = "+result);
+}
+else
+{
+long result = Calculator(firstNum, secondNum, mathOperation);
+PrintResult(firstNum + mathOperation + secondNum +" = "+result);
+}
+
+
+
---
+++ Sem4Task27/Program.cs
@@ -0,0 +1,55 @@
+﻿// №27 Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+// (задание со *) Сделать оценку времени алгоритма через вещественные числа и строки
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine()??"0"); 
+    return num;
+}
+
+// Печатаем результат
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+// Суммирование через целые числа
+int SumOfNum(int num)
+{
+    int sum = 0;
+    int numShort = num;
+    while(numShort>9)
+    {
+        sum = sum + numShort % 10;
+        numShort = numShort/10;
+    }
+    sum = sum+numShort;
+    return sum;
+}
+
+// Суммироввание через строки
+int SumStringNum(string number)
+{
+    char[] digits = number.ToCharArray();
+    int sum = 0;
+    for(int i=0; i< number.Length;i++)
+    {
+       sum = sum + int.Parse(digits[i].ToString()); 
+    }
+    return sum;
+}
+
+int num = ReadData("Введите число");
+DateTime d1 = DateTime.Now;
+int result = SumOfNum(num);
+PrintResult("Cумма цифр числа " + num +" составляет "+result);
+Console.WriteLine(DateTime.Now-d1);
+
+DateTime d2 = DateTime.Now;
+int resultString = SumStringNum(num.ToString());
+PrintResult("Cумма цифр числа " + num +" вторым методом составляет "+resultString);
+Console.WriteLine(DateTime.Now-d1);
+
